251e55bd6a6a9a499c46b1b40dc7eeb14c373cd6
Daniel Mendoza <dan.v.mendoza@gmail.com>
2022-08-16 16:16:07 -0700

    finished assignments

---
+++ WebFun/day_2/MorningAlgo.js
@@ -0,0 +1,59 @@
+// VARIABLES
+
+// DECLARING A VARIABLE
+var full_name;
+
+// INITIALIZATING A VARIBALE
+
+full_name = "Daniel Mendoza"
+
+// TOGETHER
+
+var full_name = "Daniel Mendoza" // String data type
+var age = 31 // Intergers
+var awake = false // Boolean
+var sleep = true // Boolean
+var height = 4.11 // float
+
+// ADDITION
+
+age = age + 1
+age += 1
+
+// SUBTRACTION
+
+age = age -1
+age -= 1
+
+// MULTIPLICATION & DIVISION
+
+age = age *1
+age *= 1
+age = age / 1
+age /=1
+
+console.log(age)
+
+// CONCATENAATION
+
+var age = "31"
+var number = 2
+
+console.log(age + number)
+
+// LOOPS
+
+// FOR LOOPS
+// DRY -- Dont Repeat Yourself
+
+//---varibale----condition----increment
+//-----1----------2-------3
+for(var i = 0; i < 100; i++){
+//----4 ---code body
+console.log(i)
+}
+//1 => 2 => 4 => 3
+
+
+
+
