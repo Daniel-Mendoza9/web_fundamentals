bad04c0024a69d47a480ec4cd0378d4200301827
Daniel Mendoza <dan.v.mendoza@gmail.com>
2022-09-13 12:36:47 -0700

    Initial commit

---
+++ Python/great_number_game/server.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, session, request, redirect, url_for
+import random
+
+app = Flask(__name__)
+app.secret_key = 'I bet you cant guess this'
+
+@app.route('/')
+def index():
+    if not 'number' in session:
+        session['number'] = random.randint(1, 100)
+    return render_template('index.html')
+
+@app.route('/guess', methods=['POST'])
+def guess():
+    num = session['number']
+    session['guess'] = request.form['guess']
+    guess = int(session['guess'])
+    if guess > num:
+        session['response'] = 'too high'
+        return redirect('/')
+    elif guess < num:
+        session['response'] = 'too low'
+        return redirect('/')
+    else:
+        session['response'] = 'correct'
+    return redirect('/')
+
+
+@app.route('/reset')
+def reset():
+    session.clear()
+    return redirect('/')
+
+if __name__=="__main__":
+    app.run(debug=True, port=5001)
---
+++ Python/rick_and_morty_api/server.py
@@ -0,0 +1,79 @@
+from flask import Flask, render_template, url_for, redirect
+import requests
+import random as r
+from pprint import pprint
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/random')
+def random():
+    c = r.randint(1, 826)
+    l = r.randint(1, 126)
+    e = r.randint(1, 51)
+    routes = ['e', 'l', 'l', 'l', 'c', 'c', 'c', 'c', 'c', 'c', 'c',
+              'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c']
+    pick = routes[r.randint(0, 23)]
+    if pick == 'c':
+        return redirect(f'/character/{c}')
+    if pick == 'l':
+        return redirect(f'/character/{l}')
+    if pick == 'e':
+        return redirect(f'/character/{e}')
+
+
+@app.route('/characters/<int:page>')
+def characters(page):
+    url = f'https://rickandmortyapi.com/api/character?page={page}'
+    data = requests.get(url).json()
+    prev = f'{page-1}' if page > 1 else ''
+    next = f'{page+1}' if page < data['info']['pages'] else ''
+    return render_template('many.html', data=data, thing='character', prev=prev, next=next)
+
+
+@app.route('/locations/<int:page>')
+def locations(page):
+    url = f'https://rickandmortyapi.com/api/location?page={page}'
+    data = requests.get(url).json()
+    prev = f'{page-1}' if page > 1 else ''
+    next = f'{page+1}' if page < data['info']['pages'] else ''
+    return render_template('many.html', data=data, thing='location', prev=prev, next=next)
+
+
+@app.route('/episodes/<int:page>')
+def episodes(page):
+    url = f'https://rickandmortyapi.com/api/episode?page={page}'
+    data = requests.get(url).json()
+    prev = f'{page-1}' if page > 1 else ''
+    next = f'{page+1}' if page < data['info']['pages'] else ''
+    return render_template('many.html', data=data, thing='episode', prev=prev, next=next)
+
+
+@app.route('/character/<int:id>')
+def character(id):
+    url = f'https://rickandmortyapi.com/api/character/{id}'
+    data = requests.get(url)
+    return render_template('one.html', data=data.json(), thing='character')
+
+
+@app.route('/location/<int:id>')
+def location(id):
+    url = f'https://rickandmortyapi.com/api/location/{id}'
+    data = requests.get(url)
+    return render_template('one.html', data=data.json(), thing='location')
+
+
+@app.route('/episode/<int:id>')
+def episode(id):
+    url = f'https://rickandmortyapi.com/api/episode/{id}'
+    data = requests.get(url)
+    return render_template('one.html', data=data.json(), thing='episode')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
